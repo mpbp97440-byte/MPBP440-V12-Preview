f516cc46279c6ecdb9d00c5cf574397cb4a113bf
mpbp97440-byte <mpbp97440@gmail.com>
2026-06-19 18:54:37 +0200

    Add files via upload
    update major V4 mpbp440 2026/06/

---
+++ README_PATCH_V4_0.txt
@@ -0,0 +1,11 @@
+PATCH V4.0 Portail Interactif
+
+À remplacer/ajouter :
+- index.html
+- style.css
+- admin.html
+- manifest.webmanifest
+- sw.js
+- data/artists.json
+
+Après upload : attendre GitHub Pages puis faire CTRL+F5.
--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
-self.addEventListener("install",e=>{e.waitUntil(caches.open("sparetdee-v2").then(c=>c.addAll(["./","index.html","style.css","script.js","data.json"])))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const MPBP_CACHE = 'mpbp440-v4-0';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './data/artists.json',
+  './data/news.json',
+  './data/featured.json'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(MPBP_CACHE).then(cache => cache.addAll(ASSETS)).catch(()=>{}));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== MPBP_CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
